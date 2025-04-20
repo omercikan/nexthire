813bcd414a39348a95f564a0dba9d6af66c392b7
omercikan <omercikan36@gmail.com>
2025-04-21 00:13:49 +0300

    Feat(Header): Implemented main header layout and integrated subcomponents
    - Added scroll listener for fixed header behavior
    - Integrated NavList and UserItems components

---
+++ components/layout/Header.tsx
@@ -0,0 +1,88 @@
+"use client";
+
+import Image from "next/image";
+import React, { useEffect, useRef, useState } from "react";
+import Logo from "@/public/assets/images/nexthire.png";
+import NavList from "./Header/NavList";
+import UserItems from "./Header/UserItems";
+import Link from "next/link";
+import { HiMenuAlt3 } from "react-icons/hi";
+import { useDispatch, useSelector } from "react-redux";
+import { AppDispatch, RootState } from "@/lib/store";
+import { changeMenuState } from "@/lib/features/users/userMenuSlice";
+import { CSSTransition } from "react-transition-group";
+
+const Header = () => {
+  const [scroll, setScroll] = useState<boolean>(false);
+  const [breakpoint, setBreakPoint] = useState<boolean>(false);
+  const dispatch = useDispatch<AppDispatch>();
+  const { menu } = useSelector((state: RootState) => state.userMenu);
+  const menuRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setBreakPoint(window.innerWidth >= 992);
+    };
+
+    handleResize();
+    window.addEventListener("resize", handleResize);
+
+    const handleScroll = () => {
+      setScroll(window.scrollY > 60);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  return (
+    <header
+      className={`main-header transition duration-300 ${
+        scroll ? "!fixed w-full shadow-xs bg-white" : ""
+      }`}
+    >
+      <div className="main-header__wrapper">
+        <div className="navbar-brand-menu">
+          <HiMenuAlt3
+            className="min-[992px]:hidden"
+            size={26}
+            cursor="pointer"
+            onClick={() => dispatch(changeMenuState())}
+          />
+
+          <Link href="/">
+            <Image src={Logo} alt="NextHire" width={153} />
+          </Link>
+        </div>
+
+        {breakpoint && (
+          <nav className="large-navlist">
+            <NavList />
+          </nav>
+        )}
+
+        <CSSTransition
+          in={menu}
+          timeout={300}
+          unmountOnExit
+          classNames="modal-menu"
+          nodeRef={menuRef}
+        >
+          <nav className="small-navlist min-[992px]:hidden z-50">
+            <div className="small-navlist__wrapper" ref={menuRef}>
+              <NavList />
+            </div>
+          </nav>
+        </CSSTransition>
+      </div>
+
+      <UserItems />
+    </header>
+  );
+};
+
+export default Header;
