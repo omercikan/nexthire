0c866d81b0bd4e909d6a8c2bdf4f754a9bfe6ade
omercikan <omercikan36@gmail.com>
2025-06-05 00:08:20 +0300

    test: setup improvements for testing environment

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,1 +1,11 @@
-import "@testing-library/jest-dom";
+import "@testing-library/jest-dom/";
+
+jest.mock("next/navigation", () => ({
+  usePathname: jest.fn(() => "/mocked-pathname"),
+  useRouter: () => ({
+    push: jest.fn(),
+    replace: jest.fn(),
+    prefetch: jest.fn(),
+    pathname: "/mocked-pathname",
+  }),
+}));
