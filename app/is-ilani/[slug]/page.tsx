d5769cf86eb13f88804af2dd88349296520affe5
omercikan <omercikan36@gmail.com>
2025-06-24 16:04:21 +0300

    feat(job-detail): implement job detail page and related API integrations
    - Create dynamic job detail page under `app/is-ilani/[slug]`
    - Develop `job-detail` API route to fetch job details by postID
    - Build `useGetJobDetailQuery` hook to consume job detail API
    - Integrate job detail API hook into job detail page component
    - Add session cookie API route at `api/auth/session/route.ts` for cookie management
    - Implement `useCookieSetter` custom hook for setting cookies via API
    - Integrate `useCookieSetter` in `JobItem` component to manage session cookies
    - Add API documentation for "job-detail-api" and "set-cookie-token" under `docs` folder

---
+++ app/is-ilani/[slug]/page.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import { useGetJobDetailQuery } from "@/lib/redux/services/jobDetail";
+import { useParams } from "next/navigation";
+import React from "react";
+
+const JobDetail = () => {
+  const { slug } = useParams();
+  const { data, isLoading } = useGetJobDetailQuery({
+    postID: String(
+      slug
+        ?.toString()
+        .split("-")
+        .at(slug.toString().split("-").length - 1)
+    ),
+  });
+
+  return <div>JobDetail</div>;
+};
+
+export default JobDetail;
