6f0999a58760afacda618d73fdd0dbd9a4c1de80
omercikan <omercikan36@gmail.com>
2025-07-24 16:16:50 +0300

    refactor(UploadResume): extract UploadResume label and input logic into reusable components
    - Refactored `UploadResume` component to improve readability and maintainability
    - Extracted the `<label>` element into a standalone `ResumeLabel` component
    - Moved the `<input>` element logic into a reusable `ResumeInput` component
    - Added `validateResume` utility function to handle file validation and display error messages in a centralized way

--- components/pages/jobDetail/applicationModal/modalBody/resume/UploadResume.tsx
+++ components/pages/jobDetail/applicationModal/modalBody/resume/UploadResume.tsx
@@ -1,108 +1,36 @@
-import React, { ChangeEvent, useContext, useState } from "react";
+import React, { useState } from "react";
 import InformationMessage from "../../modalUI/InformationMessage";
-import { useDispatch, useSelector } from "react-redux";
-import { AppDispatch, RootState } from "@/lib/redux/store";
+import { useSelector } from "react-redux";
+import { RootState } from "@/lib/redux/store";
 import ModalControls from "../../modalControls/ModalControls";
-import validatePdfFile from "@/lib/utils/validatePdfFile";
-import { AuthContext } from "@/context/authContext";
-import useUploadResume from "@/hooks/useUploadResume";
-import { useDeleteLastResumeMutation } from "@/lib/redux/services/resumeApi";
-import { setPdfErrorMessage } from "@/lib/redux/features/applicationModal/modalData";
+import ResumeInput from "./uploadResume/ResumeInput";
+import ResumeLabel from "./uploadResume/ResumeLabel";
 
 const UploadResume = () => {
-  const {
-    uploadedFileNames,
-    PdfErrorMessage,
-    selectedResume,
-    placeholderUploadData: { fileName },
-  } = useSelector((state: RootState) => state.applicationModalData);
-  const { user } = useContext(AuthContext);
-  const { uploadResumeToApi } = useUploadResume();
-  const [deleteLastResume] = useDeleteLastResumeMutation();
-  const dispatch = useDispatch<AppDispatch>();
+  const { uploadedFileNames, resumeErrorMessage, selectedResume } = useSelector(
+    (state: RootState) => state.applicationModalData
+  );
   const [fileValue, setFileValue] = useState<string>("");
-
-  const handleUploadResume = async (e: ChangeEvent<HTMLInputElement>) => {
-    const file = e.target.files?.[0];
-
-    if (file) {
-      const isValidFile = await validatePdfFile(file);
-      setFileValue(file?.name);
-
-      if (file.type !== "application/pdf") {
-        return dispatch(
-          setPdfErrorMessage("Yalnızca PDF formatı yükleyebilirsiniz")
-        );
-      }
-
-      if (!isValidFile) {
-        dispatch(
-          setPdfErrorMessage(
-            "PDF dosyası geçersiz ya da bozuk lütfen başka bir dosya yükleyin"
-          )
-        );
-      } else if (uploadedFileNames.includes(file.name)) {
-        dispatch(setPdfErrorMessage("Bu dosyayı zaten yüklediniz"));
-      } else if (file.size >= 3 * 1024 * 1024) {
-        dispatch(
-          setPdfErrorMessage("En fazla 3 MB boyutunda dosya yükleyebilirsiniz")
-        );
-      } else if (uploadedFileNames.length >= 4) {
-        await Promise.all([
-          deleteLastResume({
-            lastResumeName: uploadedFileNames[0],
-            userID: user?.id ?? "",
-          }),
-
-          uploadResumeToApi(file, user?.id ?? ""),
-        ]);
-      } else {
-        await uploadResumeToApi(file, user?.id ?? "");
-      }
-
-      e.target.value = "";
-    }
-  };
+  const isResumeValid = !!uploadedFileNames.length && selectedResume !== "0";
 
   return (
     <div className="mt-2">
       <div className="px-6">
-        <label
-          htmlFor="resume-upload"
-          className={`!py-1.5 !px-4 !bg-transparent hover:!bg-[#EBF4FD] shadow-[0_0_0_1px_#4045ef] hover:shadow-[0_0_0_2px_#4045ef] transition-shadow duration-300 !text-[#4045ef] font-semibold rounded-full inline-block ${
-            fileName.length ? "cursor-not-allowed" : "cursor-pointer"
-          }`}
-        >
-          Özgeçmiş yükle
-        </label>
+        <ResumeLabel />
 
         <span className="text-[#00000099] text-sm block mt-1.5">
           PDF (3 MB)
         </span>
 
-        <input
-          type="file"
-          accept=".pdf"
-          id="resume-upload"
-          name="resume"
-          hidden
-          onChange={(e) => handleUploadResume(e)}
-          disabled={!!fileName.length}
-        />
-        <div className="text-[#D91B1B] text-[15px] mt-1">{PdfErrorMessage}</div>
+        <ResumeInput setFileValue={setFileValue} />
       </div>
 
       <InformationMessage />
       <ModalControls
-        isErrors={Object.keys(
-          selectedResume !== "0" && uploadedFileNames.length
-            ? {}
-            : { PdfErrorMessage }
-        )}
+        isErrors={Object.keys(isResumeValid ? {} : { resumeErrorMessage })}
         formValues={fileValue}
         extraControl={{
-          state:
-            uploadedFileNames.length && selectedResume !== "0" ? true : false,
+          state: isResumeValid ? true : false,
           message:
             uploadedFileNames.length && selectedResume === "0"
               ? "Lütfen bir özgeçmiş seçin"
---
+++ components/pages/jobDetail/applicationModal/modalBody/resume/uploadResume/ResumeLabel.tsx
@@ -0,0 +1,23 @@
+import { RootState } from "@/lib/redux/store";
+import React from "react";
+import { useSelector } from "react-redux";
+
+const ResumeLabel = () => {
+  const fileName = useSelector(
+    (state: RootState) =>
+      state.applicationModalData.placeholderUploadData.fileName
+  );
+
+  return (
+    <label
+      htmlFor="resume-upload"
+      className={`!py-1.5 !px-4 !bg-transparent hover:!bg-[#EBF4FD] shadow-[0_0_0_1px_#4045ef] hover:shadow-[0_0_0_2px_#4045ef] transition-shadow duration-300 !text-[#4045ef] font-semibold rounded-full inline-block ${
+        fileName.length ? "cursor-not-allowed" : "cursor-pointer"
+      }`}
+    >
+      Özgeçmiş yükle
+    </label>
+  );
+};
+
+export default ResumeLabel;
