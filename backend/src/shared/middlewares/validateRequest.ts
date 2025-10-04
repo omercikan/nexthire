9214294805d151ecce99b41a95e117e3068ddaee
omercikan <omercikan36@gmail.com>
2025-10-04 16:17:05 +0300

    feat(backend): add request validation middleware

---
+++ backend/src/shared/middlewares/validateRequest.ts
@@ -0,0 +1,20 @@
+import { NextFunction, Request, Response } from "express";
+import z, { ZodType } from "zod";
+
+export const validateRequest =
+  (schema: ZodType, property: "body" | "query" | "params" = "body") =>
+  (req: Request, res: Response, next: NextFunction) => {
+    try {
+      schema.parse(req[property]);
+      next();
+    } catch (error) {
+      if (error instanceof z.ZodError) {
+        return res.status(400).json(
+          error.issues.map(({ message, path }) => {
+            return { message, path: path.at(0) };
+          })
+        );
+      }
+      next(error);
+    }
+  };
