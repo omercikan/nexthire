8a59b6f387fe83da1e120958167d5bb05993e26c
omercikan <omercikan36@gmail.com>
2025-10-05 18:01:06 +0300

    feat(backend): add CORS options and cookie-parser middleware
    - Added CLIENT_URL to config for CORS origin setup
    - Enabled credentials support in CORS
    - Added cookie-parser middleware for handling cookies
    - Installed cookie-parser and @types/cookie-parser packages

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,13 +2,16 @@ import express from "express";
 import cors from "cors";
 import authCandidateRoutes from "./features/auth/candidate/routes/authCandidateRoutes.ts";
 import { errorHandler } from "./shared/middlewares/errorHandler.ts";
+import cookieParser from "cookie-parser";
+import config from "./config/index.ts";
 
 // express app
 const app = express();
 
 // global middleware
 app.use(express.json());
-app.use(cors());
+app.use(cors({ credentials: true, origin: config.client_url }));
+app.use(cookieParser());
 
 app.use("/api/auth", authCandidateRoutes);
 
--- backend/src/config/index.ts
+++ backend/src/config/index.ts
@@ -8,6 +8,7 @@ interface Config {
   database_name: string;
   jwt_secret: string;
   saltRounds: number;
+  client_url: string;
 }
 
 const config: Config = {
@@ -17,6 +18,7 @@ const config: Config = {
   database_name: String(process.env.DB_NAME),
   jwt_secret: String(process.env.JWT_SECRET),
   saltRounds: 10,
+  client_url: String(process.env.CLIENT_URL),
 };
 
 export default config;
