0be5f6f2c0dcc88212cd56bb09b2a126e3acc06b
omercikan <omercikan36@gmail.com>
2025-10-30 15:46:54 +0300

    refactor(auth): merge Candidate and Employer into single User model

---
+++ backend/src/shared/models/User.ts
@@ -0,0 +1,51 @@
+import { model, Schema } from "mongoose";
+
+const UserSchema = new Schema(
+  {
+    // Common areas
+    fullname: {
+      type: String,
+      required: true,
+      minlength: 3,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: String,
+
+    role: {
+      type: String,
+      enum: ["candidate", "employer"],
+      required: true,
+    },
+
+    // Candidate fields
+    profilePhoto: {
+      type: String,
+      required: false,
+    },
+
+    // Employer fields
+    phoneNumber: String,
+    companyName: String,
+    city: String,
+    district: String,
+    taxCity: String,
+    taxOffice: String,
+    taxNumber: String,
+    emailConsent: Boolean,
+
+    // Common areas
+    personalDataConsent: {
+      type: Boolean,
+      default: true,
+    },
+    failedAttempts: Number,
+    failedTime: Number,
+  },
+  { timestamps: true, versionKey: false }
+);
+
+export const User = model("User", UserSchema);
