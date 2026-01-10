5d44edaac1b4a2207d2bf38d295a7dc53e71a489
omercikan <omercikan36@gmail.com>
2026-01-10 21:32:24 +0300

    feat: fetch user favorite jobs alongside job listings
    - Integrated getFavorites query into job actions hook
    - Conditionally skipped favorites request for unauthenticated users
    - Enabled favorite state awareness per job item

--- frontend/features/jobs/postings/components/JobList/useJobActions.tsx
+++ frontend/features/jobs/postings/components/JobList/useJobActions.tsx
@@ -1,6 +1,9 @@
 import { useSelector } from "react-redux";
 import { useGetJobsQuery } from "../../services/jobsApi";
 import { RootState } from "@/shared/redux/store";
+import { useGetFavoritesQuery } from "../Favorite/favoritesApi";
+import { useContext } from "react";
+import { AuthContext } from "@/features/auth/authContext";
 
 const useJobActions = () => {
   const {
@@ -9,9 +12,15 @@ const useJobActions = () => {
   } = useSelector((state: RootState) => state);
   const { data, isFetching } = useGetJobsQuery({ page: currentPage });
   const jobData = filtersItem?.length ? filterData : data;
+  const { user } = useContext(AuthContext);
+
+  const { data: favoriteData } = useGetFavoritesQuery(undefined, {
+    skip: !Boolean(user),
+  });
 
   return {
     data: jobData,
+    favoriteData,
     isFetching,
     filterData,
     filtersItem,
