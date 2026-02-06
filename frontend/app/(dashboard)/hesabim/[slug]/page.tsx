f562e6fa74ec653eb8e2d983058b0b9b98e312a6
omercikan <omercikan36@gmail.com>
2026-02-06 15:12:46 +0300

    feat: integrate role-based routing for employer dashboard
    - Updated DashboardContent to filter routes by user role using AuthContext
    - Registered the new EmployerProfile component to the '/hesabim/profilim' path for employers

--- frontend/app/(dashboard)/hesabim/[slug]/page.tsx
+++ frontend/app/(dashboard)/hesabim/[slug]/page.tsx
@@ -1,14 +1,17 @@
 "use client";
 
+import { AuthContext } from "@/features/auth/authContext";
 import CandidateOverviewPage from "@/features/dashboard/candidate/OverviewPage/OverviewPage";
-import CandidateProfile from "@/features/dashboard/candidate/Profile/Profile";
+import CandidateProfile from "@/features/dashboard/candidate/Profile/CandidateProfile";
 import CandidateResume from "@/features/dashboard/candidate/Resumes/components/Resume/CandidateResume";
+import EmployerProfile from "@/features/dashboard/employer/Profile/EmployerProfile";
 import EmployerShareJob from "@/features/dashboard/employer/ShareJob/ShareJob";
 import { usePathname } from "next/navigation";
-import { useEffect } from "react";
+import { useContext, useEffect } from "react";
 
 const DashboardContent = () => {
   const pathname = usePathname();
+  const { user } = useContext(AuthContext);
 
   useEffect(() => {
     document.body.style.backgroundColor = "#F5F7FA";
@@ -28,6 +31,12 @@ const DashboardContent = () => {
     },
 
     {
+      path: "/hesabim/profilim",
+      renderComponent: <EmployerProfile />,
+      role: "employer",
+    },
+
+    {
       path: "/hesabim/ozgecmislerim",
       renderComponent: <CandidateResume />,
       role: "candidate",
@@ -40,15 +49,13 @@ const DashboardContent = () => {
     },
   ];
 
-  const activeComponent = routes.find((r) => pathname === r.path);
+  const activeComponent = routes.find(
+    (r) => pathname === r.path && user?.role === r.role,
+  );
 
   return (
     <main className="p-8 max-sm:p-4 max-sm:mt-[77.78px] bg-[#F5F7FA] relative">
       {activeComponent?.renderComponent}
-
-      {/* {pathname === "/hesabim/genel-bakis" && <CandidateOverviewPage />}
-      {pathname === "/hesabim/profilim" && <CandidateProfile />}
-      {pathname === "/hesabim/ozgecmislerim" && <CandidateResume />} */}
     </main>
   );
 };
