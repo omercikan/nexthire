6667930483b7b964d6ef433a218909e549b29e1c
omercikan <omercikan36@gmail.com>
2026-02-13 16:02:28 +0300

    chore: update dev script, dependencies, and add helmet

--- backend/src/shared/middlewares/index.ts
+++ backend/src/shared/middlewares/index.ts
@@ -2,9 +2,11 @@ import config from "../../config";
 import express, { Application } from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import helmet from "helmet";
 
 export const applyGlobalMiddlewares = (app: Application) => {
   // Security
+  app.use(helmet());
   app.use(cors({ credentials: true, origin: config.client_url }));
 
   // Parsing
