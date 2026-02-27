ff87bff5697dd63ea115c01a6fed0576c856b4a2
omercikan <omercikan36@gmail.com>
2026-02-27 17:41:42 +0300

    feat: Created `ChatIcons` object to centralize all chat-related icons
    - Includes icons for chat, minimize, close, options, emoji, attach, send, new chat, CV, history, fullscreen
    - Facilitates consistent usage across Chat components

---
+++ frontend/features/chat/icons/ChatIcon.js
@@ -0,0 +1,22 @@
+import { FiMinus, FiMoreHorizontal, FiX, FiPaperclip } from "react-icons/fi";
+import { BsChatSquareDotsFill } from "react-icons/bs";
+import { FaRegFaceSmileBeam } from "react-icons/fa6";
+import { BiSolidSend } from "react-icons/bi";
+import { IoChatboxEllipsesOutline } from "react-icons/io5";
+import { PiReadCvLogo } from "react-icons/pi";
+import { TbHistoryToggle } from "react-icons/tb";
+import { RxExitFullScreen } from "react-icons/rx";
+
+export const ChatIcons = {
+  options: FiMoreHorizontal,
+  mninimize: FiMinus,
+  close: FiX,
+  chat: BsChatSquareDotsFill,
+  smile: FaRegFaceSmileBeam,
+  attach: FiPaperclip,
+  send: BiSolidSend,
+  newChat: IoChatboxEllipsesOutline,
+  cv: PiReadCvLogo,
+  history: TbHistoryToggle,
+  screen: RxExitFullScreen,
+};
