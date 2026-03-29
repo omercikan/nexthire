f12e47889e65fbbeaec13715a3ae75d48ae99607
omercikan <omercikan36@gmail.com>
2026-03-29 22:16:31 +0300

    fix(application-summary): hide screening questions section when no questions exist

--- frontend/features/job-detail/components/applicationModal/modalBody/ApplicationSummary.tsx
+++ frontend/features/job-detail/components/applicationModal/modalBody/ApplicationSummary.tsx
@@ -3,8 +3,14 @@ import SummaryResume from "./applicationSummary/summaryResume/SummaryResume";
 import SummaryContent from "./applicationSummary/SummaryContent";
 import SummaryQuestions from "./applicationSummary/additionalQuestions/SummaryQuestions";
 import ModalFooter from "./ModalFooter";
+import { useSelector } from "react-redux";
+import { RootState } from "@/shared/redux/store";
 
 const ApplicationSummary = () => {
+  const { screeningQuestions } = useSelector(
+    (state: RootState) => state.applicationModalData.applicationData,
+  );
+
   return (
     <div className="h-[calc(100%-225.58px)] sm:h-125 overflow-auto">
       <div className="px-6 max-sm:px-3">
@@ -19,9 +25,11 @@ const ApplicationSummary = () => {
       <ContactInformation />
       <SummaryResume />
 
-      <SummaryContent contentTitle="Ek Sorular" step={3}>
-        <SummaryQuestions />
-      </SummaryContent>
+      {screeningQuestions.length ? (
+        <SummaryContent contentTitle="Ek Sorular" step={3}>
+          <SummaryQuestions />
+        </SummaryContent>
+      ) : null}
 
       <ModalFooter isValid={true} />
     </div>
