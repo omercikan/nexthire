b8141f96ed75611470c69940534c2b0c9d5ac69c
omercikan <omercikan36@gmail.com>
2026-04-14 14:59:54 +0300

    refactor(StatCardItem): enhance card with delta indicators
    - add delta and isPositiveDelta props
    - display trend icons (up/down)
    - add conditional styling for positive/negative values
    - support loading state

--- frontend/features/dashboard/employer/Overview/components/StatCard/StatCardItem.tsx
+++ frontend/features/dashboard/employer/Overview/components/StatCard/StatCardItem.tsx
@@ -1,26 +1,68 @@
 import React from "react";
 import { IconType } from "react-icons/lib";
+import { statIcons } from "../../icons/statIcons";
+import { cn } from "@/shared/libs/utils";
 
 interface StatCardItemProps {
   icon: IconType;
   dataText: string;
+  delta: number;
+  isPositiveDelta: boolean;
   dataDescription: string;
   dataSubDescription: string;
+  isLoading: boolean;
 }
 
 const StatCardItem: React.FC<StatCardItemProps> = (props) => {
-  const { icon: Icon, dataText, dataDescription, dataSubDescription } = props;
+  const {
+    icon: Icon,
+    dataText,
+    delta,
+    isPositiveDelta,
+    dataDescription,
+    dataSubDescription,
+    isLoading,
+  } = props;
 
   return (
     <div className="flex-1 max-sm:text-center border border-[#e2e5e8] bg-white shadow-lg rounded-2xl py-6 px-5">
-      <div className="bg-[#E6F1FB] p-2.5 rounded-xl w-max max-sm:mx-auto">
-        <Icon color="0073d5" size={20} />
+      <div className="flex justify-between items-center">
+        <div className="bg-[#E6F1FB] p-2.5 rounded-xl w-max max-sm:mx-auto">
+          <Icon color="0073d5" size={20} />
+        </div>
+
+        {!isLoading ? (
+          <div
+            className={cn(
+              "flex items-center gap-1 font-medium text-xs",
+              isPositiveDelta ? "text-[#5bb661]" : "text-[#cc272e]",
+            )}
+          >
+            {isPositiveDelta ? (
+              <statIcons.TrendingUp />
+            ) : (
+              <statIcons.TrendingDown />
+            )}
+            {isPositiveDelta ? "+" : "-"}
+            {delta.toString().replace("-", "")}
+          </div>
+        ) : (
+          <div className="animate-pulse rounded-md bg-gray-300 w-10 h-5" />
+        )}
       </div>
 
       <div className="mt-4">
-        <p className="text-[#0f171f] text-2xl font-bold">{dataText}</p>
-        <p className="text-[#5b646f] text-sm mt-1 whitespace-nowrap">{dataDescription}</p>
-        <p className="text-[#5b646f] text-sm mt-1 whitespace-nowrap">{dataSubDescription}</p>
+        {!isLoading ? (
+          <p className="text-[#0f171f] text-2xl font-bold">{dataText}</p>
+        ) : (
+          <p className="animate-pulse rounded-md bg-gray-300 w-7 h-7" />
+        )}
+        <p className="text-[#5b646f] text-sm mt-1 whitespace-nowrap">
+          {dataDescription}
+        </p>
+        <p className="text-[#5b646f] text-sm mt-1 whitespace-nowrap">
+          {dataSubDescription}
+        </p>
       </div>
     </div>
   );
