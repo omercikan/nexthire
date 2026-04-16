3886ab9513acaacd15d2618ecc219efa62e548b4
omercikan <omercikan36@gmail.com>
2026-04-16 16:14:34 +0300

    feat(employer/jobs): add EmployerJobsPanel container component

---
+++ frontend/features/dashboard/employer/Jobs/EmployerJobsPanel.tsx
@@ -0,0 +1,11 @@
+import JobListingsHeader from "./Header/JobListingsHeader";
+
+const EmployerJobsPanel = () => {
+  return (
+    <main className="container">
+      <JobListingsHeader />
+    </main>
+  );
+};
+
+export default EmployerJobsPanel;
