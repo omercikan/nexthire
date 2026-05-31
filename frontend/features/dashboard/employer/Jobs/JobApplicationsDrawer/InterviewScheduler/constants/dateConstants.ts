646fa5647e3ed2fe04af3af187ea98d39489781e
omercikan <omercikan36@gmail.com>
2026-05-31 17:54:03 +0300

    feat(interview-scheduler): add date and status color constants

---
+++ frontend/features/dashboard/employer/Jobs/JobApplicationsDrawer/InterviewScheduler/constants/dateConstants.ts
@@ -0,0 +1,48 @@
+export const WEEK_DAYS = ["Pt", "Sa", "Ça", "Pe", "Cu", "Ct", "Pz"] as const;
+
+export const WEEK_DAYS_FULL = [
+  "Pazartesi",
+  "Salı",
+  "Çarşamba",
+  "Perşembe",
+  "Cuma",
+  "Cumartesi",
+  "Pazar",
+] as const;
+
+export const MONTHS = [
+  "Ocak",
+  "Şubat",
+  "Mart",
+  "Nisan",
+  "Mayıs",
+  "Haziran",
+  "Temmuz",
+  "Ağustos",
+  "Eylül",
+  "Ekim",
+  "Kasım",
+  "Aralık",
+] as const;
+
+export const TIME_SLOTS = [
+  "09:00",
+  "09:30",
+  "10:00",
+  "10:30",
+  "11:00",
+  "11:30",
+  "12:00",
+  "12:30",
+  "13:00",
+  "13:30",
+  "14:00",
+  "14:30",
+  "15:00",
+  "15:30",
+  "16:00",
+  "16:30",
+  "17:00",
+  "17:30",
+  "18:00",
+] as const;
---
+++ frontend/features/dashboard/employer/Jobs/JobApplicationsDrawer/InterviewScheduler/constants/statusColors.ts
@@ -0,0 +1,13 @@
+export const statusBackgroundColors = {
+  green: "bg-green-500",
+  yellow: "bg-yellow-400",
+  orange: "bg-orange-500",
+  red: "bg-red-500",
+};
+
+export const statusTextColors = {
+  green: "text-green-500",
+  yellow: "text-yellow-400",
+  orange: "text-orange-500",
+  red: "text-red-500",
+};
