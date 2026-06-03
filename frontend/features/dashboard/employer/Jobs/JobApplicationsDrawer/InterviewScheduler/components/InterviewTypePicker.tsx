7aee20032402cb3408781564d40a88d7621d3348
omercikan <omercikan36@gmail.com>
2026-06-03 15:34:40 +0300

    feat(interview-scheduler): add InterviewTypePicker component

---
+++ frontend/features/dashboard/employer/Jobs/JobApplicationsDrawer/InterviewScheduler/components/InterviewTypePicker.tsx
@@ -0,0 +1,48 @@
+import CustomButton from "@/shared/components/ui/CustomButton";
+import { AppDispatch, RootState } from "@/shared/redux/store";
+import { BsCameraVideo } from "react-icons/bs";
+import { LuBuilding2 } from "react-icons/lu";
+import { useDispatch, useSelector } from "react-redux";
+import { setType } from "../interviewSchedulerSlice";
+import { cn } from "@/shared/libs/utils";
+
+type PickerType = "online" | "in_person";
+
+const getButtonClassName = (
+  buttonType: PickerType,
+  selectedType: PickerType | null,
+) => {
+  return cn(
+    "flex justify-center items-center gap-2 border rounded-lg! py-2.5! px-3 text-sm font-medium bg-transparent! border-border",
+    selectedType === buttonType
+      ? "text-[#0073d5]! border-[#0073d5]! bg-primary/5!"
+      : "text-muted-foreground! hover:border-foreground/20! hover:text-foreground!",
+  );
+};
+
+const InterviewTypePicker = () => {
+  const dispatch = useDispatch<AppDispatch>();
+  const type = useSelector((state: RootState) => state.interviewScheduler.type);
+
+  return (
+    <div className="grid grid-cols-2 gap-2">
+      <CustomButton
+        className={getButtonClassName("online", type)}
+        handleClick={() => dispatch(setType("online"))}
+      >
+        <BsCameraVideo size={16} strokeWidth={0.5} />
+        Online
+      </CustomButton>
+
+      <CustomButton
+        className={getButtonClassName("in_person", type)}
+        handleClick={() => dispatch(setType("in_person"))}
+      >
+        <LuBuilding2 size={16} />
+        Yüz Yüze
+      </CustomButton>
+    </div>
+  );
+};
+
+export default InterviewTypePicker;
