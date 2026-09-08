b1b6663e1a6ac49bb34155b5e9517b79394c3a18
omercikan <omercikan36@gmail.com>
2026-09-08 18:33:51 +0300

    feat(identity): add identity mapper

---
+++ services/identity-service/src/main/java/com/nexthire/identity/mapper/IdentityMapper.java
@@ -0,0 +1,19 @@
+package com.nexthire.identity.mapper;
+
+import com.nexthire.identity.Role;
+import com.nexthire.identity.entity.Identity;
+import org.springframework.stereotype.Component;
+
+@Component
+public class IdentityMapper {
+
+    public Identity toEntity(String email, String hashedPassword, Role role) {
+        Identity identityEntity = new Identity();
+
+        identityEntity.setEmail(email);
+        identityEntity.setHashedPassword(hashedPassword);
+        identityEntity.setRole(role);
+
+        return identityEntity;
+    }
+}
