cca37ed2948eb73583bd653b9bf245e4ee6e77c9
omercikan <omercikan36@gmail.com>
2026-09-15 18:57:50 +0300

    feat(user): listen for candidate registration events

---
+++ services/user-service/src/main/java/com/nexthire/user/messaging/listener/UserRegistrationListener.java
@@ -0,0 +1,22 @@
+package com.nexthire.user.messaging.listener;
+
+import com.nexthire.user.messaging.event.CandidateCreatedEvent;
+import com.nexthire.user.service.UserService;
+import lombok.AllArgsConstructor;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+@Component
+@AllArgsConstructor
+public class UserRegistrationListener {
+
+    private final UserService userService;
+
+    @RabbitListener(
+            queues = "candidate.queue",
+            containerFactory = "candidateRabbitListenerContainerFactory"
+    )
+    public void candidateRegistrationListen(CandidateCreatedEvent event) {
+        userService.createCandidate(event);
+    }
+}
